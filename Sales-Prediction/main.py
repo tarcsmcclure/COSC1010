a84a8be8bee7400528b587a4eebefe7a7876fbdb
Anna McClure <annamcclure@westernwyoming.edu>
2025-02-12 02:34:06 +0000

    Ready for grading

--- Sales-Prediction/main.py
+++ Sales-Prediction/main.py
@@ -1,14 +1,19 @@
 #
-# Name
-# Date
+# Anna McClure
+# 2/11/25
 # Sales Prediction Programming Project
 # COSC 1010
 #
 
 # Variables to hold the sales total and the profit
+total_sales = 0
+profit = 0
 
 # Get the amount of projected sales.
+total_sales = float(input('Enter the projected sales: '))
 
 # Calculate the projected profit.
+profit = total_sales * 0.23
 
 # Print the projected profit.
+print('The profit is $', format(profit, ',.2f'))
