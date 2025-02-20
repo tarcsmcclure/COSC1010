b9e98d92ec1ffe301c0223ffe4068cf019a08fcb
Anna McClure <annamcclure@westernwyoming.edu>
2025-02-20 16:14:10 -0700

    Update main.py

--- Areas-of-Rectangles/main.py
+++ Areas-of-Rectangles/main.py
@@ -1,21 +1,38 @@
 #
-# Name
-# Date
+# Anna McClure
+# 2/20/25
 # Areas of Rectangles Programming Project
 # COSC 1010
 #
 # Local variables
+length1 = 0
+length2 = 0
+width1 = 0
+width2 = 0
 
 # Get length A
+length1 = int(input('Enter the length of rectangle 1: '))
 
 # Get width A
+width1= int(input('Enter the width of rectangle 1: "))
 
 # Get length B
+length2 = int(input('Enter the length of rectangle 2: '))
 
 # Get width B
+width1= int(input('Enter the width of rectangle 2: "))
 
 # Calculate area A
+area1 = length1 * width1
 
 # Calculate area B
+area2 = length2 * width2
 
 # Print area comparison using if-elif-else statements
+if area1 > area2:
+  print('Rectangle 1 has the greater area.')
+else:
+  if area2 > area1:
+    print('Rectangle 2 has the greater area.')
+  else:
+    print('Bothhave the same area.')
