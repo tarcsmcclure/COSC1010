45bc2b8da3ca754473de085930026ec8f7ff0ffa
Anna McClure <annamcclure@westernwyoming.edu>
2025-03-02 18:31:43 +0000

    Ready for re-grading

--- Budget-Analysis/main.py
+++ Budget-Analysis/main.py
@@ -1,4 +1,3 @@
-#
 # Anna McClure
 # 2/27/25
 # Budget Analysis Programming Project
@@ -8,20 +7,22 @@ budget = 0
 bills = 0
 total_bills = 0
 amt_left = 0
-
-#Set the inputs to get the total budget and the amount of the 1st bill to start the loop.
+#Ask user for monthly budget
 budget = float(input('Enter your budget for the month in dollars $'))
+#Ask for the first bill
 bills = float(input('Enter the amount of the bill, enter 0 to end. $'))
-
-#Start the while loop, while the bills isn't 0 which would end the loop the user will keep getting prompted.
+#This loops until the user enters a 0, indicating no more bills
 while bills != 0:
-    #This is the prompt until 0 is entered to end the loop
+    total_bills += bills #this adds the bills up
+    amt_left = budget - total_bills # this calculates the amount of money left after paying bills
+    #IF statement to check if the bills exceed the budget
+    if total_bills > budget:
+        print('You do not have enough money to pay your bills')
+        break #the break makes it so it stops prompting for bills if you do not have enough moneey
+    #This prompts for the next bill, which indicates the user has enough money to continue
     bills = float(input('Enter the amount of your next bill, enter 0 to end. $'))
-#Adds all the bills together to give total bills
-    total_bills += bills
-#Calculates the amount left in budget
-    amt_left = budget - total_bills
-#prints the total of the bills and the amount left after paying all bills
-print('The total amount of your monthly bills is $',total_bills, 'the amount you will have left after paying bills is $',amt_left)
-
+#The print statements show the amount of money left and the total of all the bills
+print('The total amount of your monthly bills is $',total_bills)
+print('the amount you will have left after paying bills is $',amt_left)
+        
 # Use comments liberally throughout the program. 
