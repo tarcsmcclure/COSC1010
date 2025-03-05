e22d05bdddfc7a783163ad1702f3025c88fbd03b
Anna McClure <annamcclure@westernwyoming.edu>
2025-03-05 15:13:57 +0000

    Ready for grading

--- Feet-to-Inches/main.py
+++ Feet-to-Inches/main.py
@@ -1,7 +1,23 @@
 #
-# Name
-# Date
+# Anna McClure
+# 3-5-25
 # Feet to Inches Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program.
+#constant for the number of inches per feet
+INCHES_PER_FOOT = 12
+
+#main function
+def main():
+    #get a number of feet from user
+    feet = int(input('Enter the number of feet: '))
+    
+    #convert to inches
+    print(feet,'feet', 'equals', feet_to_inches(feet), 'inches')
+
+#the feet_to_inches function converts
+def feet_to_inches(feet):
+    return feet * INCHES_PER_FOOT
+    
+#call main
+main()
