f095b2db0829cfd32b8d872a47ad42d893c21cfb
Anna McClure <annamcclure@westernwyoming.edu>
2025-03-29 13:31:22 -0600

    Update main.py

--- File-Display/main.py
+++ File-Display/main.py
@@ -1,7 +1,17 @@
 #
-# Name
-# Date
+# Anna McClure
+# 3/29/25
 # File Display Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+#Open the file
+myfile = open('numbers.txt', 'r')
+
+#read and display the file contents
+for line in myfile:
+  number = int(line)
+  print(number)
+
+#close the file
+myfile.close()
