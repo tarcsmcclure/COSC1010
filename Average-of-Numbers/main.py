d8d630e440fa44de8961253b89da62edc02ee152
Anna McClure <annamcclure@westernwyoming.edu>
2025-03-30 10:11:25 -0600

    Update main.py

--- Average-of-Numbers/main.py
+++ Average-of-Numbers/main.py
@@ -1,4 +1,4 @@
-#Assume a file containing a series of integers is named `numbers.txt` and exists on the computer’s disk. 
+#Assume a file containing a series of integers is named `numbers.txt` and exists on the computerâ€™s disk. 
 #Write a program that calculates the average of all the numbers stored in the file.
 # Anna McClure
 # 3/29/25
@@ -6,17 +6,22 @@
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+#begin program
 def main():
+    #initilize sum variable
     sum = 0.0
     #open the numbers file
     with open ('numbers.txt', 'r') as numbers_file:
-        #read the first number in file
+        #begin reading data in file
         for line in numbers_file:
+            #variable named total set to float for each line
             total = float(line)
+            #variable sum adds numbers together
             sum += total
-
+            
+    #prints results
     print(f'{sum:,.2f}')
-
+#calls function
 if __name__ == '_main_':
     main()
       
